b993d63e75e7314d74e9360950549e58d9b4a407
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-05-25 15:26:46 -0500

    add styling

--- user_app/src/user.js
+++ user_app/src/user.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function User(props) {
     const { user } = props
     return (
-        <div className='user container'>
-            <h2>{user.first_name} {user.last_name}</h2>
-            <p>{user.email}</p>
-        </div>
+            <div className='user'>
+               <h2>{user.first_name} {user.last_name}</h2>
+                <p>{user.email}</p> 
+            </div>
     )
 }
